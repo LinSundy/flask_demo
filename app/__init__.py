72a07a9253147bf405fc9f273ade3fe34c91f0cb
chelin <chelin>
2020-02-27 16:14:37 +0800

    增加部分注释

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,10 @@ from .setting import envs
 
 def create_app(env):
     app = Flask(__name__)
+    # 加载配置
     app.config.from_object(envs.get(env))
+    # 初始化第三方库
     init_ext(app)
+    # 初始化路由
     init_views(app=app)
     return app
